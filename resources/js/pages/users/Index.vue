45e41b8326508b143b41609dc214b6929d1c057e
Polash Mahmud <polashmahmud@gmail.com>
2025-09-21 16:43:03 +0600

    refactor: integrate DataTable with Pinia store for dynamic column management

--- resources/js/pages/users/Index.vue
+++ resources/js/pages/users/Index.vue
@@ -3,8 +3,10 @@ import AppLayout from '@/layouts/AppLayout.vue';
 import { Head } from '@inertiajs/vue3';
 import { type BreadcrumbItem } from '@/types';
 import DataTable from '@/components/table/DataTable.vue';
-import { ref } from 'vue';
+import { useDataTableStore } from '@/stores/datatable.js';
+import { onMounted } from 'vue';
 
+const dataTableStore = useDataTableStore();
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -21,18 +23,22 @@ defineProps({
     users: Object
 })
 
-const columns = ref([
+const columns = [
     { name: 'ID', field: 'id', sortable: true },
     { name: 'Name', field: 'name', sortable: true },
     { name: 'Email', field: 'email', sortable: true },
     { name: 'Created At', field: 'created_at', sortable: true },
-]);
+];
+
+onMounted(() => {
+    dataTableStore.setColumns(columns);
+});
 </script>
 
 <template>
 
     <Head title="Users" />
     <AppLayout :breadcrumbs="breadcrumbs">
-        <DataTable :data="users" :columns="columns" />
+        <DataTable :data="users" :columns="dataTableStore.columns" />
     </AppLayout>
 </template>
